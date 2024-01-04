0c6bcfbfe1f97e58f3c3961d385fbc0ba9788019
Edward Baitsewe <edward@monatemedia.com>
2024-01-04 23:36:09 +0200

    Set document root to public, import CSS and JavaScript, helper file set up

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo 'Hello World!';
-
-// Command to start dev server
-// php -S localhost:8000
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Command to start dev server
+// php -S localhost:8000 -t public
+
+require '../helpers.php';
+
+require basePath('views/home.view.php');
