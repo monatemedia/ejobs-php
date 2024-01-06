771e0b215fabeb5b1cbb757265ccd21c601eaaa0
Edward Baitsewe <edward@monatemedia.com>
2024-01-06 14:22:42 +0200

    Create a VERY Basic Router

--- public/index.php
+++ public/index.php
@@ -5,4 +5,18 @@
 
 require '../helpers.php';
 
-loadView('home');
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php',
+];
+
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if(array_key_exists($uri, $routes)) {
+    require(basepath($routes[$uri]));
+} else {
+    require basepath($routes['404']);
+}
