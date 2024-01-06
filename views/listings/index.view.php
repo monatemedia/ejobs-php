b76af1014a5ad5f70898c983f34f6fcd1b6b76b7
Edward Baitsewe <edward@monatemedia.com>
2024-01-06 15:06:27 +0200

    Create Views

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,6 +1,5 @@
 <?= loadPartial('head')?>
 <?= loadPartial('navbar')?>
-<?= loadPartial('showcase-search')?>
 <?= loadPartial('top-banner')?>
 
 
@@ -190,10 +189,6 @@
             </div>
           </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-          <i class="fa fa-arrow-alt-circle-right"></i>
-          Show All Jobs
-        </a>
       </section>
 
       <?= loadPartial('bottom-banner')?>
