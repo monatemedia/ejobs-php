66870ec8c7c5e64fefe19649d474148721000405
Edward Baitsewe <edward@monatemedia.com>
2024-02-26 23:56:52 +0200

    Query Method & Fetching Listings

--- public/index.php
+++ public/index.php
@@ -4,19 +4,18 @@
 // php -S localhost:8000 -t public
 
 require '../helpers.php';
-require basePath('Database.php');
-$config = require basePath('config/db.php');
-
-$db = new Database($config);
-//inspect($db);
-
 require basePath('Router.php');
+require basePath('Database.php');
 
+//Instantiate the router
 $router = new Router();
 
+//Get routes
 $routes = require basePath('routes.php');
 
+//Get the current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+//Route the request
 $router->route($uri, $method);
